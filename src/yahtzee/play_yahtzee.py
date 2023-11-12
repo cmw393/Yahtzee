4e1a194d8b493f87da11c90d1b58680ae4e93f70
Collin Wilkinson <cmw393@drexel.edu>
2023-11-12 14:54:07 -0500

    had chatgpt redo the file one atribute error

--- src/yahtzee/play_yahtzee.py
+++ src/yahtzee/play_yahtzee.py
@@ -1,93 +1,69 @@
 from player import Player
-from display_dice import display_dice
-from scoring import Scoring
 
-class PlayYahtzee(Player):
-    def __init__(self, num_players=1):
-        self.num_players = num_players
-        self.players = [Player() for _ in range(num_players)]
-        self.current_player_index = 0
-        
+class PlayYahtzee:
+    def __init__(self):
+        self.player = Player()
 
-    def next_player(self):
-        """Switch to the next player for their turn."""
-        self.current_player_index = (self.current_player_index + 1) % self.num_players
+    def roll(self):
+        print("\nStarting a new turn")
+        self.player.turn = self.player.PlayerTurn(self.player)
+        print("Turn created successfully")
+        print("Score card:", self.player.turn.scoring.get_score_card())
+        rolled_values = self.player.turn.roll()
+        print("Rolled values:", rolled_values)
 
-    def roll(self, player):
-        """Initiates a dice roll and displays the results."""
-        print("\nRolling the dice...")
-        player.turn.roll()
-        display_dice(player.turn.get_state()['dice_values'])
+    def choose_category(self):
+        print("\nChoosing a category")
+        valid_categories = self.player.turn.scoring.remaining_categories()
+        print("Available Categories:", valid_categories)
+        category = input("Choose a scoring category: ")
 
-    def choose_category(self, player):
-        """Allows the player to choose a scoring category."""
-        print("\nChoosing a scoring category...")
-        player.display_score_card()
-        available_categories = player.remaining_categories()
+        if category not in valid_categories:
+            print("Invalid category. Please choose a valid category.")
+            self.choose_category()
+        else:
+            score = self.player.turn.scoring.calculate_score(category, self.player.turn.get_state()['dice_values'])
+            self.player.turn.scoring.mark_score(category, score)
+            print(f"Scored {score} points in {category}!")
 
-        for category in available_categories:
-            score = player.score.calculate_score(category, player.turn.get_state()['dice_values'])
-            print(f"{category}: {score}")
+    def start_new_turn(self):
+        print("\nStarting a new turn")
+        self.player.turn = self.player.PlayerTurn(self.player)
+        print("Turn created successfully")
 
-        category_input = input("Enter the category to score: ")
-        while category_input not in available_categories:
-            print("Invalid category. Choose from the remaining categories.")
-            category_input = input("Enter the category to score: ")
-
-        score = player.score.calculate_score(category_input, player.turn.get_state()['dice_values'])
-        player.score.mark_score(category_input, score)
-
-
-    def start_new_turn(self, player):
-        """Resets the game state for a new turn."""
-        player.turn.rolls_left = 3
-        player.held_dice = []
-
-    def turn(self, player):
-        """Manages a single turn in the game."""
-        for roll_num in range(3):
-            print(f"\nRoll {roll_num + 1}")
-            self.roll(player)
-            held_indices = player.turn.get_state()['held_dice']
-            display_dice(player.turn.get_state()['dice_values'], held_indices)
-
-            if roll_num < 2:  # Allow holding dice only for the first two rolls
-                hold_input = input("Enter indices of dice to hold (space-separated, or 'none' to hold none): ")
-                if hold_input.lower() != 'none':
-                    indices_to_hold = [int(index) for index in hold_input.split()]
-                    player.turn.hold(indices_to_hold)
-
-            self.choose_category(player)
+    def turn(self):
+        print("\nTaking a turn")
+        self.roll()
+        self.hold_dice_prompt()
+        self.roll()
+        self.hold_dice_prompt()
+        self.roll()
+        self.choose_category()
 
     def play_game(self):
-        """The main game loop that calls other methods to progress the game."""
-        rounds = 13  # Number of rounds in a Yahtzee game
-
-        for _ in range(rounds):
-            print(f"\nRound {_ + 1}")
-
-            for player in self.players:
-                print(f"\n{player.name}'s turn:")
-                self.start_new_turn(player)
-                self.turn(player)
-
+        print("\nPlaying the game")
+        while not self.player.turn.scoring.is_full():
+            self.start_new_turn()
+            self.turn()
         self.game_over()
 
     def generate_block_art(self):
-        """Generates ASCII art for the game-over screen."""
-        print("##########")
-        print("# GAME OVER #")
-        print("##########")
+        print("Congratulations!")
+        # Add your ASCII art here
 
     def game_over(self):
-        """Ends the game and displays the final score."""
+        print("\nGame Over!")
+        self.player.display_score_card()
         self.generate_block_art()
-        print("\nFinal Scores:")
-        for player in self.players:
-            print(f"{player.name}: {sum(player.score.get_score_card().values())}")
+        exit()
 
-# Run the game
+    def hold_dice_prompt(self):
+        indices = input("Enter indices of dice to hold (e.g., 1 3 5), or press Enter to roll all dice: ")
+        if indices:
+            indices = [int(idx) - 1 for idx in indices.split()]
+            self.player.turn.hold(indices)
+
+
 if __name__ == "__main__":
-    num_players = int(input("Enter the number of players: "))
-    yahtzee_game = PlayYahtzee(num_players)
+    yahtzee_game = PlayYahtzee()
     yahtzee_game.play_game()
--- src/yahtzee/player.py
+++ src/yahtzee/player.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List
 from scoring import Scoring
 from dice import YahtzeeDice
 from display_dice import display_dice
@@ -9,53 +9,46 @@ class Player(Scoring):
         self.name = input("Enter your name: ")
         self.score = 0
         self.turn = self.PlayerTurn(self)
-        self.score = Scoring()
 
     class PlayerTurn:
-            def __init__(self, player_instance):
-                self.yahtzee_dice = YahtzeeDice()  # Assuming you have a YahtzeeDice class
-                self.rolls_left = 3
+        def __init__(self, player_instance):
+            self.yahtzee_dice = YahtzeeDice()  # Assuming you have a YahtzeeDice class
+            self.rolls_left = 3
+            self.held_dice = []
+            self.player_instance = player_instance
+            self.scoring = Scoring()
+        def roll(self):
+            """
+            Rolls all dice not being held, updates their values, and decrements rolls_left.
+            """
+            if self.rolls_left > 0:
+                # Determine indices of dice not being held
+                dice_to_roll = [i for i in range(self.yahtzee_dice.num_dice) if i not in self.held_dice]
+
+                # Roll the remaining dice
+                rolled_values = self.yahtzee_dice.roll(keep=[False] * self.yahtzee_dice.num_dice)
+
+                # Update held_dice and rolls_left
                 self.held_dice = []
-                self.player_instance = player_instance
-            
-            def roll(self):
-                """
-                Rolls all dice not being held, updates their values, and decrements rolls_left.
-                """
-                if self.rolls_left > 0:
-                    # Determine indices of dice not being held
-                    dice_to_roll = [i for i in range(self.yahtzee_dice.num_dice) if i not in self.held_dice]
+                self.rolls_left -= 1
 
-                    # Roll the remaining dice
-                    rolled_values = self.yahtzee_dice.roll(keep=[False] * self.yahtzee_dice.num_dice)
+                return rolled_values
+            else:
+                print("No more rolls left in this turn.")
+                return []
 
-                    # Update held_dice and rolls_left
-                    self.held_dice = []
-                    self.rolls_left -= 1
+        def hold(self, indices: List[int]):
+            """
+            Accepts a list of indices and updates held_dice to hold those dice.
+            """
+            self.held_dice = indices
 
-                    return rolled_values
-                else:
-                    print("No more rolls left in this turn.")
-                    return []
-            
-            def hold(self, indices: List[int]):
-                """
-                Accepts a list of indices and updates held_dice to hold those dice.
-                """
-                self.held_dice = indices
-            
-            def get_state(self):
-                """
-                Returns a dictionary containing the current state of the turn (dice_values, rolls_left, held_dice).
-                """
-                return {
-                    'dice_values': self.yahtzee_dice.get_values(),
-                    'rolls_left': self.rolls_left,
-                    'held_dice': self.held_dice
-                }
-            
-    def __init__(self):
-        super().__init__()
-        self.name = input("Enter your name: ")
-        self.score = 0
-        self.turn = self.PlayerTurn(self)
+        def get_state(self):
+            """
+            Returns a dictionary containing the current state of the turn (dice_values, rolls_left, held_dice).
+            """
+            return {
+                'dice_values': self.yahtzee_dice.get_values(),
+                'rolls_left': self.rolls_left,
+                'held_dice': self.held_dice
+            }
